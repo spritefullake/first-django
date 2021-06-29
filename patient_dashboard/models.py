d51536772ae93bb71eb72cf532f245c343b5f846
Talha Ahmad <42ahmad@gmail.com>
2021-06-29 09:46:19 -0700

    added blood sample model

--- patient_dashboard/models.py
+++ patient_dashboard/models.py
@@ -12,3 +12,29 @@ class Patient(models.Model):
 
     def __str__(self):
         return f"{self.age}"
+
+class BloodSample(models.Model):
+    patient = models.ForeignKey(
+        Patient,
+        on_delete = models.CASCADE
+    )
+    vessel_type = models.TextField()
+    collection_date = models.DateTimeField()
+    notes = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    # blood draw fields:
+    temperature = models.FloatField()
+    spO2 = models.FloatField()
+    heart_rate = models.FloatField()
+    blood_pressure = models.FloatField()
+    weight = models.FloatField() #should this go to patient?
+    blood_draw_volume = models.FloatField()
+
+class CommercialAnalysis(models.Model):
+    pass
+class ChronusAnalysis(models.Model):
+    pass
+class CMPCommercialAnalysis(models.Model):
+    pass
+class CMPChronusAnalysis(models.Model):
+    pass
